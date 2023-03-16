9982c6353b5989cead325420c0e47fb5e9997d28
nixx1we <BrickMix1961@abv.bg>
2023-03-16 20:07:49 +0200

    novo movo

--- The World Of Metox/Assets/MovementController.cs
+++ The World Of Metox/Assets/MovementController.cs
@@ -7,8 +7,19 @@ public class MovementController : MonoBehaviour
     [SerializeField] public float speed = 8f;
     [SerializeField] private Rigidbody rb;
 
+    public float gravity;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+    
+
     private void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
         var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
         rb.velocity = dir * speed;
